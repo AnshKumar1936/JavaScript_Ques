faf5c1443fe883b10df0c40e86f9bb8729e61a96
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-01-15 23:14:23 +0530

    Add files via upload

---
+++ 1to100Even.js
@@ -0,0 +1,5 @@
+for(let i=1;i<=100;i++){
+    if(i%2==0){
+        console.log("1 to 100 even numbers are:", i);
+    }
+}
---
+++ VoteOrNot.js
@@ -0,0 +1,7 @@
+let a=20;
+
+if(a>18){
+    console.log("You can vote");
+}else{
+    console.log("You cannot vote");
+}
