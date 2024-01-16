3db3f6d3be84b87c620002fb3e0946d39867a774
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-01-16 21:37:04 +0530

    Add files via upload

---
+++ NoOfVowels.js
@@ -0,0 +1,24 @@
+// function vowels(str){
+//     let count=0;
+//     for( const char of str){
+//     if(char==="a" || char==="e" || char==="i" || char==="o" || char=="u" ){
+//         count++;
+//     }
+// }
+// console.log(count);
+// }
+
+// vowels("ansh"); 
+
+const vowels=(str)=>{
+    let count=0;
+    for( const char of str){
+    if(char==="a" || char==="e" || char==="i" || char==="o" || char=="u" ){
+        count++;
+    }
+}
+console.log(count);
+
+}
+
+vowels("ansh");
