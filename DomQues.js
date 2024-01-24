1ad8d2ccfd860b214b18392018aefdce645c1425
Ansh Kumar Singh <148905627+AnshKumar1936@users.noreply.github.com>
2024-01-24 19:08:57 +0530

    Add files via upload

---
+++ DomQues.js
@@ -0,0 +1,8 @@
+let newBtn= document.createElement("button");
+newBtn.innerText = "click me";
+newBtn.style.backgroundColor="red";
+newBtn.style.color="white";
+newBtn.style.fontSize= "20px";
+
+
+document.querySelector("body").prepend(newBtn);
